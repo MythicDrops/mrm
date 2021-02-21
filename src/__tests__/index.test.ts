157cd864328019093a067ad9d5ad1547dce074fd
Richard Harrah <topplethenunnery@gmail.com>
2021-02-20 20:32:56 -0500

    test(branches): update tests for multiple branches

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -4,6 +4,6 @@ it("applies 6 plugins", () => {
   expect(plugins.length).toBe(6);
 });
 
-it("only releases on the main branch", () => {
-  expect(branches).toEqual(["main"]);
+it("releases on the main branch", () => {
+  expect(branches).toContain(["main"]);
 });
