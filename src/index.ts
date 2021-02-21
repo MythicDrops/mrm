1fb3ea8e6b16f69e6dd2ba15bbadc6eb3c98a578
Richard Harrah <topplethenunnery@gmail.com>
2021-02-20 20:14:13 -0500

    feat(github): upload distribution zips to GitHub releases

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,9 @@ export const plugins = [
         {
           path: "**/build/libs/*.jar",
         },
+        {
+          path: "**/build/distributions/*.zip",
+        },
       ],
     },
   ],
